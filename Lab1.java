44ed9bfdc53eb3f6180ab5b8d95575ad47f04396
Odzen <49286935+Odzen@users.noreply.github.com>
2020-10-12 06:52:57 +0300

    Some changes to the fuctions

--- Lab1.java
+++ Lab1.java
@@ -5,12 +5,12 @@ public class Lab1
 {
     public static void main(String args [])
     {
-        short d[]=new short[10]; 
+        short c[]=new short[10]; 
         double x[]=new double[11];
-        double z[][]= new double[10][11];
+        double d[][]= new double[10][11];
 
-        for(int i=0,  val =21; i<d.length;i++, val-=2)
-            d[i]=(short)val;
+        for(int i=0,  val =21; i<c.length;i++, val-=2)
+            c[i]=(short)val;
 
         for(int i=0; i<x.length;i++)
             x[i]=randomNumber();
@@ -22,26 +22,28 @@ public class Lab1
      {
         for (int j=0;j<11;j++)
         {
-            if(d[i]==7)
-                z[i][j]=firstOp(x[j]);
-            else if (d[i]==5 ||d[i]==11 || d[i]==13|| d[i]==19 || d[i]==21)
-                z[i][j]=secondOp(x[j]);
+            
+            if(c[i]==7)
+                d[i][j]=firstOp(x[j]);
+            else if (c[i]==5 ||c[i]==11 || c[i]==13|| c[i]==19 || c[i]==21)
+                d[i][j]=secondOp(x[j]);
             else
-                z[i][j]=thirdOp(x[j]);
+                d[i][j]=thirdOp(x[j]);
 
-            System.out.print(String.format("%.4f", z[i][j]) + " | ");
+            System.out.print(String.format("%.4f", d[i][j]) + " | ");
 
         }
         
         System.out.println();            
         System.out.println();
      }
-     
-    /* Print for tests 
-     printShort(d);
+
+    /*Print for tests 
+     printShort(c);
 
      printDouble(x);
-     */     
+     */
+          
 
     }
 
@@ -59,22 +61,20 @@ public class Lab1
         double cubicRoot=Math.pow(x,1.0/3.0);
         double numerator=1.0/2.0;
         double denominator= Math.pow(cubicRoot,1.0/3.0);
-        double value= Math.pow((numerator/denominator),Math.tan(cubicRoot));
+        double value= Math.pow((numerator/denominator),Math.tan(Math.toRadians(cubicRoot)));
         
         return value; 
     }
 
     public static double secondOp(double x)
     {
-        double innerOp= Math.sin(Math.pow((4.0/x),x));
-        double value= Math.sin(innerOp);
-        
-        return value;
+        double innerOp= Math.sin(Math.toRadians(Math.pow((4.0/x),x))); 
+        return Math.sin(Math.toRadians(innerOp));
     }
 
     public static double thirdOp(double x)
     {
-        double numerator=Math.sin(Math.pow(x, ((Math.PI-x)/x)));
+        double numerator=Math.sin(Math.toRadians(Math.pow(x, ((Math.PI-x)/x))));
         double denominator=2.0;
         double innerOp= Math.pow((numerator/denominator),2.0);
         double value=Math.pow(innerOp,1.0/3.0);
@@ -82,24 +82,24 @@ public class Lab1
         return value;
     }
 
-    /* auxiliary functions for printing and testing
+    /*auxiliary functions for printing and testing
 
     public static void printShort(short[] a)
     {
         for(int i=0; i<a.length;i++)
-        {
             System.out.println(a[i]);
-        }
+        System.out.println("\n");
     }
 
     public static void printDouble(double[] a)
     {
         for(int i=0; i<a.length;i++)
-        {
             System.out.println(a[i]);
-        }
+        System.out.println("\n");
+        
     }
     */
+    
 
 }
 
